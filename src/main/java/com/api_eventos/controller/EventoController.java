a1abd575272c481c55d9e036629e27ffd9e26536
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-09-14 11:36:22 -0300

    feat: controllers evento e inscrição

--- src/main/java/com/api_eventos/controller/EventoController.java
+++ src/main/java/com/api_eventos/controller/EventoController.java
@@ -1,10 +1,36 @@
 package com.api_eventos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api_eventos.dto.EventoDto;
+import com.api_eventos.dto.EventoResponse;
+import com.api_eventos.model.Evento;
+import com.api_eventos.service.EventoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/eventos")
 public class EventoController {
 
+    @Autowired
+    private EventoService eventoService;
+
+    @GetMapping
+    public List<EventoResponse> listar(){
+        return eventoService.listar();
+    }
+
+    @PostMapping
+    public ResponseEntity<EventoResponse> cadastrar(@RequestBody EventoDto evento){
+        EventoResponse eventosalvo = eventoService.cadastrar(evento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(eventosalvo);
+    }
+
+    @GetMapping("/{id}")
+    public EventoResponse listarPorId(@PathVariable Long id){
+        return eventoService.buscarId(id);
+    }
 }
--- src/main/java/com/api_eventos/controller/ParticipanteController.java
+++ src/main/java/com/api_eventos/controller/ParticipanteController.java
@@ -1,9 +1,37 @@
 package com.api_eventos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api_eventos.dto.EventoDto;
+import com.api_eventos.dto.EventoResponse;
+import com.api_eventos.dto.ParticipanteDto;
+import com.api_eventos.dto.ParticipanteResponse;
+import com.api_eventos.service.EventoService;
+import com.api_eventos.service.ParticipanteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/paticipante")
+@RequestMapping("/participante")
 public class ParticipanteController {
+    @Autowired
+    private ParticipanteService participanteService;
+
+    @GetMapping
+    public List<ParticipanteResponse> listar(){
+        return participanteService.listar();
+    }
+
+    @PostMapping
+    public ResponseEntity<ParticipanteResponse> cadastrar(@RequestBody ParticipanteDto participante){
+        ParticipanteResponse participantesalvo = participanteService.cadastrar(participante);
+        return ResponseEntity.status(HttpStatus.CREATED).body(participantesalvo);
+    }
+
+    @GetMapping("/{id}")
+    public ParticipanteResponse listarPorId(@PathVariable Long id){
+        return participanteService.buscarId(id);
+    }
 }
