267de6581a03319d8a12e0ea87c2490079b9368d
kauana-santos <160909870+kauana-santos@users.noreply.github.com>
2026-09-15 11:37:43 -0300

    inscrição duplicada

--- src/main/java/com/api_eventos/repository/InscricaoRepository.java
+++ src/main/java/com/api_eventos/repository/InscricaoRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
     Long countByEventoId(Long id);
+    boolean existsByEventoIdAndParticipanteId(Long eventoId, Long participanteId);
 }
